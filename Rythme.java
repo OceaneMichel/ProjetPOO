d43612be94f481f5f33ad191cf7a61eb4ab12862
OceaneMichel <oceane.michel.poly@gmail.com>
2015-12-17 01:43:29 +0100

    Update Rythme.java

--- Rythme.java
+++ Rythme.java
@@ -1,5 +1,14 @@
 package poo;
 
-public class Rythme {
-
+public class Rythme implements Comparable<Rythme>{
+	int BPM;
+	
+	public Rythme(int i){
+		BPM = i;
+	}
+	
+	public float comparer(Rythme r){
+		return (float)(100-Math.abs((r.BPM-BPM)))/100;
+		
+	}
 }
