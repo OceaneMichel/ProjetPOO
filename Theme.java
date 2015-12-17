85a854d326cea4a5e2aed9682661d21f7a5bf772
OceaneMichel <oceane.michel.poly@gmail.com>
2015-12-17 01:42:00 +0100

    Update Theme.java

--- Theme.java
+++ Theme.java
@@ -1,5 +1,13 @@
 package poo;
-
+/*
+ * Thèmes à trier : 
+ * Amour, amitié, émotions, animaux, joie, tristesse, 
+ * haine, mort, justice, drogue, alcol, enfance, souvenirs,
+ * vie, nature, femmes, hommes, humouristique, argent, ecologie,
+ * nourriture, ville, transport, voyage, mariage, jeux vidéos,
+ * rêves, temps qui passe...
+ */
 public class Theme {
-
+	private String nom;
+	private String Catégorie;
 }
