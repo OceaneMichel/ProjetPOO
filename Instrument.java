6e0b2cddd5ee6d19a88f76de2a022a0fd302fb0b
Léo Garreau <leo.gar@hotmail.fr>
2015-12-27 14:03:54 +0100

    Update Instrument.java

--- Instrument.java
+++ Instrument.java
@@ -1,5 +1,15 @@
 package poo;
 
 public class Instrument {
-
+	private String nom;
+	private int id;
+	
+	public Instrument(String n, int instr){
+		nom=n;
+		id=instr;
+	}
+	
+	public String toString(){
+		return nom;
+	}
 }
