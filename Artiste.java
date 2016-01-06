9f97ccb443fd28f59ebb9ad3899698c3e85a9e6a
Leo <leo.gar@hotmail.fr>
2016-01-06 15:17:26 +0100

    Commit last update

--- Artiste.java
+++ Artiste.java
@@ -1,38 +1,13 @@
 package poo;
 
 public class Artiste {
-	private int id;
-	private String nom;
-	private String prenom;
-
-	public Artiste(){}
+	private int idAr;
+	private String nomAr;
+	private String type;
 	
-	public Artiste(int id_a, String n, String p){
-		id=id_a;
-		nom=n;
-		prenom=p;
-	}
-	public String getPrenom() {
-		return prenom;
-	}
-	
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	
-	public String getNom() {
-		return nom;
-	}
-	
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
+	public Artiste(int id, String nom, String t){
+		idAr = id;
+		nomAr = nom;
+		type = t;
 	}
 }
--- Instrument.java
+++ Instrument.java
@@ -1,15 +1,5 @@
 package poo;
 
 public class Instrument {
-	private String nom;
-	private int id;
-	
-	public Instrument(String n, int instr){
-		nom=n;
-		id=instr;
-	}
-	
-	public String toString(){
-		return nom;
-	}
+
 }
