5e8cc50fd21529a5452096f3eae2399043534864
OceaneMichel <oceane.michel.poly@gmail.com>
2016-01-07 14:39:35 +0100

    Create Genre.java

---
+++ Genre.java
@@ -0,0 +1,83 @@
+package poo;
+
+import java.util.ArrayList;
+
+
+public class Genre implements Comparable<Genre> {
+	private int dist_genres[]; // Détermine la distance entre les genres
+	public ArrayList<Sous_style> fils;
+	protected String nom;
+	protected int id;
+	
+	/**
+	 * Constructeur de Genre
+	 * @param n nom du Genre
+	 */
+	public Genre(String n){
+		nom = n;
+		fils = new ArrayList<Sous_style>();
+	}
+	
+	/**
+	 * Constructeur de Genre n°2
+	 * @param n Nom du Genre
+	 * @param i indice du Genre
+	 */
+	public Genre(String n, int i){
+		dist_genres = new int[100];
+		nom = n;
+		id = i;
+		dist_genres[id] = 0;
+		fils = new ArrayList<Sous_style>();
+	}
+	
+	/**
+	 * Définit la distance entre deux Genres 
+	 * @param s le Genre
+	 * @param distance La distance entre les Genres
+	 */
+	public void set_distance(Genre s, int distance){
+		this.dist_genres[s.id] = distance;
+	}
+	
+	/**
+	 * Ajoute un sous_style à l'ArrayListe du Genre contenant ses fils
+	 * @param s le sous_style à ajouter
+	 */
+	public void add_sstyle(Sous_style s){
+		fils.add(s);
+	}
+	
+	/**
+	 * Le nombre de Fils du Genre
+	 * @return Renvoie le nombre de fils du Genre (int)
+	 */
+	public int nb_fils(){
+		return fils.size();
+	}
+	
+	/**
+	 * Affiche le Genre au format : 
+	 * [Genre] a nbfils fils.
+	 * Ses Sous_styles sont : ssstyle1 ssstyle2...
+	 */
+	public void afficher(){
+		int i;
+		System.out.print("["+nom+ ": "+ nb_fils() + " fils]");
+		System.out.print(" Ses fils sont : ");
+		for(i=0; i<fils.size(); i++){
+			System.out.print(fils.get(i).nom+" ");
+		}
+		System.out.print("\n\n");
+	}
+	
+
+	//--- COMPARAISON ---
+	public float comparer(Genre s){
+		return this.dist_genres[s.id];
+	}
+	
+	public float comparer(Sous_style s){
+		return 0;
+	}
+}
