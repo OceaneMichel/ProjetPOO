2a71fab2bc8a4b6c35c3a0cf4f23486f36697024
OceaneMichel <oceane.michel.poly@gmail.com>
2016-02-11 09:23:01 +0100

    Update Comparable.java

--- Comparable.java
+++ Comparable.java
@@ -1,11 +1,13 @@
 package poo;
 
+import java.util.ArrayList;
+
 public interface Comparable<T> {
 	/**
 	 * Chaque classe implémentant l'interface Comparable devra 
 	 * pouvoir rendre un pourcentage (float) de ressemblance
 	 * @param element
-	 * @return Le pourcentage de ressemblance
+	 * @return Le pourcentage de ressemblance : float entre 0 et 1
 	 */
 	public float comparer(T element);
 }
